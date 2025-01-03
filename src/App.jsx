44295b74f3891a356fade4e1a9cf7985fc03cdf4
Omarzizo16 <midomero941@gmail.com>
2025-01-03 18:09:58 +0200

    sec commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ let routers =  createBrowserRouter([
   {path:'' , element:<Layout />, children:[
     {index:true , element:<Home />},
     {path:'Home' , element:<Home />},
-    {path:'   ' , element:<Home />},
+    {path:'Start-Framework/' , element:<Home />},
     {path:'About' , element:<About />},
     {path:'Portfolio' , element:<Portfolio />},
     {path:'Contact' , element:<Contact />},
